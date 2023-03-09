ad4a7ecb1ad1f079febba1e992c4faf6e296ac0b
RajBusa <20it011@charusat.edu.in>
2023-03-10 00:29:32 +0530

    Model, userController and videoController are completed

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,44 @@
-import express from "express";
-import mongoose from "mongoose";
-import dotenv from "dotenv"
+import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import userRoutes from './routes/users.js'
+import authRoutes from './routes/auth.js'
+import commentRoutes from './routes/comments.js'
+import videoRoutes from './routes/videos.js'
+import cookieParser from 'cookie-parser'
 
-const app = express();
-dotenv.config();
+const app = express()
+dotenv.config()
 
 const connect = () => {
-    mongoose.connect(process.env.MONGO).then(() => {
-        console.log("Connected to MongoDB")
-    }).catch((error) => {
-        throw error;
-    });
+  mongoose
+    .connect(process.env.MONGO)
+    .then(() => {
+      console.log('Connected to MongoDB')
+    })
+    .catch((error) => {
+      throw error
+    })
 }
 
-app.listen(8080, () =>{
-    connect();
-    console.log("Connected to port 8080");
-});
+app.use(express.json())
+app.use(cookieParser());
+app.use('/api/users', userRoutes)
+app.use('/api/comments', videoRoutes)
+app.use('/api/videos', commentRoutes)
+app.use('/api/auth', authRoutes)
+
+app.use((err, req, res, next) => {
+    const status = err.status || 500;
+    const message = err.message || "Something went wrong!";
+    return res.status(status).json({
+      success: false,
+      status,
+      message,
+    });
+  });
+
+app.listen(8080, () => {
+  connect()
+  console.log('Connected to port 8080')
+})
---
+++ backend/routes/videos.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import {} from "../controllers/video.js";
+
+const router = express.Router();
+router.post("/", verifyToken, addVideo)
+router.put("/:id", verifyToken, addVideo)
+router.delete("/:id", verifyToken, addVideo)
+router.get("/find/:id", getVideo)
+router.put("/view/:id", addView)
+router.get("/trend", trend)
+router.get("/random", random)
+router.get("/sub",verifyToken, sub)
+router.get("/tags", getByTag)
+router.get("/search", search)
+export default router;
